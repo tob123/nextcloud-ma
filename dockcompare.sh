b78e7bb757fef59b2fe545c52f436c017689c5e2
tob <tob@nice.eu>
2020-05-16 15:36:13 +0200

    correct prod repo url

--- dockcompare.sh
+++ dockcompare.sh
@@ -46,17 +46,17 @@ tag_push () {
 	docker buildx build \
 	--platform linux/amd64,linux/arm/v7 \
 	--build-arg NC_VER=${VERSION} \
-	--tag tob123/${PROD_REPO}:${VERSION} \
-	--tag tob123/${PROD_REPO}:${VERSION_MAJOR} \
+	--tag ${PROD_REPO}:${VERSION} \
+	--tag ${PROD_REPO}:${VERSION_MAJOR} \
 	--push --progress plain nc
 }
 tag_push_latest () {
 	docker buildx build \
 	--platform linux/amd64,linux/arm/v7 
 	--build-arg NC_VER=${VERSION} \
-	--tag tob123/${PROD_REPO}:${VERSION} \
-	--tag tob123/${PROD_REPO}:${VERSION_MAJOR} \
-	--tag tob123/${PROD_REPO}:latest \
+	--tag ${PROD_REPO}:${VERSION} \
+	--tag ${PROD_REPO}:${VERSION_MAJOR} \
+	--tag ${PROD_REPO}:latest \
 	--push --progress plain nc
 }
 
